fa9ec4b95b7e5195105bf29017cdd1c6f24be4c8
LewMatt <lewmateush@gmail.com>
2022-05-04 16:41:28 +0200

    files updated

--- BikeRental.Service/BikeRentalService.cs
+++ BikeRental.Service/BikeRentalService.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace BikeRental.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    
     public class BikeRentalService : IBikeRentalService
     {
         
---
+++ BikeRental.Service/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace BikeRental.Service
+{
+    [DataContract]
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string UserType { get; set; }
+
+    }
+}
