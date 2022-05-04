0f1ad99717a90ac55257423495c6a3ae1d07f4bc
thunderblowpl@gmail.com <thunderblowpl@gmail.com>
2022-05-04 21:34:45 +0200

    Menu

---
+++ BikeRental.ClientApp/UserControlBorrowed.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BikeRental.ClientApp
+{
+    partial class UserControlBorrowed
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelBorrowed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelBorrowed
+            // 
+            this.labelBorrowed.AutoSize = true;
+            this.labelBorrowed.Location = new System.Drawing.Point(256, 42);
+            this.labelBorrowed.Name = "labelBorrowed";
+            this.labelBorrowed.Size = new System.Drawing.Size(102, 13);
+            this.labelBorrowed.TabIndex = 0;
+            this.labelBorrowed.Text = "Moje Wypożyczenia";
+            // 
+            // UserControlBorrowed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelBorrowed);
+            this.Name = "UserControlBorrowed";
+            this.Size = new System.Drawing.Size(584, 370);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelBorrowed;
+    }
+}
